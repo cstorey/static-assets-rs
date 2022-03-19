3d497202b5013429238a10fc77a1fbb9171025a5
Ceri Storey <cez@necrofish.org.uk>
2022-03-19 12:14:36 +0000

    Show that it works in axum.

--- hyper/tests/smoke.rs
+++ hyper/tests/smoke.rs
@@ -1,8 +1,10 @@
 use anyhow::{Context, Result};
+use axum::{routing::get_service, Router};
 use hyper::{
     header::{ETAG, IF_NONE_MATCH},
     Body, Request, StatusCode,
 };
+use log::warn;
 use tower::ServiceExt;
 
 use static_assets_hyper::{static_assets, StaticService};
@@ -90,5 +92,41 @@ async fn should_serve_with_revalidation() -> Result<()> {
 
     assert_eq!(parts.status, StatusCode::NOT_MODIFIED);
 
+    Ok(())
+}
+
+#[tokio::test]
+async fn should_serve_nested_in_axum() -> Result<()> {
+    env_logger::try_init().unwrap_or_default();
+
+    let srv = StaticService::new(&ASSETS);
+
+    let app = Router::new().nest(
+        "/static",
+        get_service(srv).handle_error(|error| async move {
+            warn!("Error serving request: {}", error);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Internal error: {}", error),
+            )
+        }),
+    );
+
+    let resp = app
+        .oneshot(
+            Request::builder()
+                .uri("/static/canary.html")
+                .body(Body::empty())?,
+        )
+        .await
+        .unwrap();
+
+    println!("Resp: {:?}", resp);
+    assert!(resp.status().is_success());
+
+    let body = hyper::body::to_bytes(resp.into_body()).await?;
+    let bodystr = std::str::from_utf8(&body).context("utf8 body")?;
+
+    assert_eq!(bodystr, "<p>Hi!</p>\n");
     Ok(())
 }
